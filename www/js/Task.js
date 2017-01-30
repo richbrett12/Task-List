fed36921adc3f3866f1987450a80ccecee58f314
Brett Rich <Brett Rich>
2017-01-30 14:20:04 -0500

    Pseudocode for Task class

--- www/js/Task.js
+++ www/js/Task.js
@@ -1,3 +1,14 @@
 class Task {
-	
+// data
+	// description: description of task
+	// time estimate: how long task will take to do
+	// isComplete: is the task finished
+	// priority: how important this task is in relation to others
+	// list item: the actual DOM element
+// methods
+	// alert: alert user to complete task
+	// edit description:
+	// edit time estimate:
+	// mark as done:
+	// mark as undone:	
 }
